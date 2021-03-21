dbb95dcd4f4830742f59e626767a3762e4344849
tshub <tshub@DESKTOP-UKRR9NB>
2021-03-21 09:48:25 +0530

    commit

---
+++ LabWork/src/com/cg/labwork/Lab6/Exercise7.java
@@ -0,0 +1,25 @@
+package com.cg.labwork.Lab6;
+
+import java.util.Arrays;
+
+public class Exercise7 {
+	public static int[] getSorted(int[] arr) {
+		int n = arr.length;
+		int temp[] = new int[n];
+		for(int i = 0; i < n; i++) {
+			String str = Integer.toString(arr[i]);
+			StringBuilder sb1 = new StringBuilder();
+			sb1.append(str);
+			sb1.reverse();
+			temp[i] = Integer.parseInt(sb1.toString());
+		}
+		Arrays.sort(temp);
+		return temp;
+	}
+	public static void main(String[] args) {
+		int arr[] = new int[] {21,25,65,78,24,34,89,105};
+		int result[] = getSorted(arr);
+		System.out.println(Arrays.toString(result));
+	}
+	
+}
