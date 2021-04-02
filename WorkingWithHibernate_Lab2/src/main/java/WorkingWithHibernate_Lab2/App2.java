2a88121799ae8693a1eb15e2e9027b2077415a24
tshub <tshub@DESKTOP-UKRR9NB>
2021-04-02 18:23:30 +0530

    commit

--- WorkingWithHibernate_Lab2/src/main/java/WorkingWithHibernate_Lab2/App2.java
+++ WorkingWithHibernate_Lab2/src/main/java/WorkingWithHibernate_Lab2/App2.java
@@ -17,11 +17,36 @@ public class App2 {
 
 		em.getTransaction().begin();
 		
+		System.out.println("Query 1");
 		Query query1 = em.createQuery("Select title from Book");
 		List<String> bookList = query1.getResultList();
 		
 		bookList.forEach(e -> System.out.println(e));
 
+		System.out.println("Query 2");
+		System.out.print("Enter the author name: ");
+		String authorName = sc.nextLine();
+		Query query2 = em.createQuery("select a.books from Author a where a.name = '" + authorName+ "'");
+		List<Book> list2 = query2.getResultList();
+		for (Book s : list2) 
+			System.out.println(s);
+		
+
+		System.out.println("c.	List all books with given price range. e.g. between Rs. 100 to 500");
+		Query query3 = em.createQuery("select b from Book b where b.price between 100 and 500");
+		List<Book> list3 = query3.getResultList();
+		for (Book s : list3) 
+			System.out.println(s);
+		
+
+		System.out.println("d. List the author name for given book id. ");
+		Query query4 = em.createQuery("select b.authors from Book b where b.isbn=1");
+		List<Author> list4 = query4.getResultList();
+		for (Author s : list4) 
+			System.out.println(s);
+		
+		
+		
 		em.getTransaction().commit();
 		
 		
